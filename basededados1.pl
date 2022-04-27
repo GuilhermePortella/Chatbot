977eb5c719bfae13cd263d960da07cdcc1332d93
guilhermeportella <guilhermeportella2@gmail.com>
2022-04-27 01:03:23 -0300

    initial commit

---
+++ basededados1.pl
@@ -0,0 +1,75 @@
+basedeDados1('O que e uma rede de computadores?',
+'redes de computadores sao conjuntos de dispositivos computacionais conectados em um mesmo sistema de comunicacao, o que possibilita a troca de informacoes e recursos').
+
+basedeDados1('Qual a classificacao de redes?' ,
+'Redes podem ser classificadas quanto a area geografica coberta. Tres classes de redes podem ser identificadas:Redes Locais (LANs); Redes Metropolitanas (MANs); e
+Redes de Longa Distancia (WANs).').
+
+basedeDados1('O que e uma lan?' ,
+'LAN (Local Area Network) corresponde as redes locais de empresas,
+escritorios, laboratorios de informatica ou ate domesticas. Em sintese, e restrita
+a uma pequena area.').
+
+basedeDados1('O que e Man?' ,
+'MAN (Metropolitan Area Network) diz respeito as redes que tem alcance
+metropolitano. Ou seja, uma rede que interconecta recursos computacionais
+em uma area referente a uma metropole.').
+
+basedeDados1('O que e wan?',
+'WAN (Wide Area Network) e uma rede que interconecta recursos computacionais distantes geograficamente uns dos outros. Nessas redes sao colocados equipamentos que
+interligam cidades e ate continentes.').
+
+basedeDados1('O que e um roteador?',
+'Elemento utilizado para encaminhar pacotes de informacoes ao destino adequado.').
+
+basedeDados1('O que e uma placa de rede?',
+'Elemento empregado como interface entre o computador e o cabeamento da rede, conhecido tambem como adaptador.').
+
+basedeDados1('O que e um Modem?',
+'Elemento utilizado para modular o sinal digital para que possa ser transmitido.').
+
+basedeDados1('O que e um repetidor?',
+'O repetidor, por sua vez, pega o sinal gerado pelo roteador e o replica, criando uma segunda rede com o sinal de internet mais amplificado e com maior alcance em comparação com o roteador operando sozinho.').
+
+basedeDados1('O que e um Switch?',
+'Um switch é um dispositivo que simplesmente conecta todos os elementos da sua rede. Ele atua como ponte ou unidade de controle para que computadores, impressoras, servidores e todos os outros tipos dispositivos possam se comunicar.').
+
+basedeDados1('O que e internet?',
+'A internet é uma rede mundial que tem como objetivo interligar computadores para fornecer ao usuário o acesso a diversas informações. Por isso é chamada de rede mundial de computadores.').
+
+basedeDados1('O que e Correio eletronico?',
+'email ou correio eletrônico, é um sistema de comunicação baseado no envio e recebimento de mensagens eletrônicas através de computadores pela Internet').
+
+basedeDados1('O que e um Cabo coaxial?',
+'O Cabo Coaxial é usado principalmente para fins audiovisuais. As residências modernas normalmente estão equipadas com pelo menos uma tomada de cabo coaxial em cada cômodo. Isso ocorre porque as empresas de TV cabo dependem dela para entregar o sinal aos clientes').
+
+basedeDados1('O que e um Cabo de par trancado?',
+' O cabeamento por par trançado (Twisted pair) é um tipo de cabo que tem um par fios entrelaçados um ao redor do outro para cancelar as interferências eletromagnéticas de fontes externas e interferências mútuas (linha cruzada ou, em inglês, crosstalk) entre cabos vizinhos.').
+
+basedeDados1('O que e a Fibra optica?',
+'A fibra ótica é uma estrutura de vidro cilíndrica, transparente, flexível e com dimensões microscópicas, parecidas com um fio de cabelo. É uma forma de transmissão que permite o tráfego de dados com velocidades próximas à velocidade da luz.').
+
+basedeDados1('O que e Radiofrequência?',
+'  São correntes alternadas de alta frequência que passam pelos cabos condutores e que chegando até as antenas onde são convertidas em ondas eletromagnéticas (OEM) e irradiadas pelo ar.').
+
+basedeDados1('O que sao Topologias de redes?',
+'Topologia é o termo usado para definir a forma como você estrutura a sua rede de computadores').
+
+basedeDados1('O que e uma Topologia Parcialmente Ligada?',
+' Nesse tipo de tecnologia, os computadores são interligados de forma intermediária, ou seja, não são todos os computadores que são interligados, embora, em caso de problemas com a conexão física, haja caminhos alternativos para se chegar até a outra máquina.').
+
+basedeDados1('O que e uma Topologia em Barramento?',
+'A topologia em barramento é de fácil instalação, pois basta que o backbone passe nas proximidades do nó para ser possível fazer a conexão. Assim, essa topologia usa uma quantidade menor de cabos que as topologias de malha e anel, por exemplo.').
+
+basedeDados1('O que e uma Topologia Ponto a Ponto?',
+'Esse tipo de topologia é chamada de ponto-a-ponto, ou P2P, já que, em cada "ponta", ou seja, cada máquina - também chamada de nó, pode ser tanto um cliente, quanto um servidor -podem transmitir e receber dados sem precisar de um servidor central.').
+
+basedeDados1('O que e uma Topologia Multiponto?',
+' Na topologia Ponto Multiponto, a rede permite o alcance de vários assinantes, limitando o número de roteadores, switches e outros equipamentos que são necessários para o funcionamento de uma rede cabeada').
+
+basedeDados1('O que e uma Topologia em Estrela?',
+' É o tipo de configuração mais comum. A rede é organizada de forma que os nós sejam conectados a um hub central, que atua como um servidor. O hub gerencia a transmissão de dados pela rede. Ou seja, qualquer dado enviado pela rede viaja pelo hub central antes de terminar em seu destino.').
+
+basedeDados1('sair', 'Fim').
+
+basedeDados1(_, 'Este item nao existe. Desculpe').
---
+++ chatbot.pl
@@ -0,0 +1,32 @@
+% Universidade Cruzeiro do Sul
+% Ciencia da Computacao
+% Disciplina: Projeto Integrador de Competencias em Ciencia da Computacao
+% Nome: Guilherme Portella
+
+
+% working_directory(CVD, 'C:/Users/Guilherme/Desktop/chatbot').
+% [chatbot].
+%  projetoIntegrador.
+% 'rede de computadores'.
+
+% referência ao banco de dados
+:- include('basededados1.pl').
+:- include('basededados2.pl').
+
+projetoIntegrador :-
+  write('---------------BEM VINDO AO FUTURO---------------'), nl,
+  write('Digite uma pergunta LEMBRE-SE LETRAS SEM ACENTUACAO E ENTRE '' E COM . :..........'), nl,
+
+  repeat, nl,
+  read(Input),
+ % data_base_search(Input, Output),
+ search(Input, Output),
+  write(Output), nl,
+  end_session(Input).
+
+search(Input, Answer) :-
+  basedeDados2(Input, Answer), !;
+  basedeDados1(Input, Answer), !.
+
+end_session(Input) :-
+  Input = sair.
